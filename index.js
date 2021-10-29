137e18898064418183c0646b356507af01c534e7
Abdus salam <salam.ads97@gmail.com>
2021-10-29 22:49:40 +0600

    modified

--- index.js
+++ index.js
@@ -6,15 +6,13 @@ require('dotenv').config()
 
 
 const app = express();
-const port = 5000;
+const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Runing Genius Server');
-});
+
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.oiptd.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
@@ -69,6 +67,9 @@ async function run() {
 }
 run().catch(console.dir);
 
+app.get('/', (req, res) => {
+    res.send('Runing Genius Server');
+});
 
 app.listen(port, () => {
     console.log('Running Genius Server on port', port);
